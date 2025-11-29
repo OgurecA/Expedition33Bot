3f5c8d51b9906c28dd94f7a236009d0fb839cef4
OgurecA <acpwork044@gmail.com>
2025-11-30 03:00:58 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 
 # ----------------- настройки -----------------
-CONF        = 0.7
+CONF        = 0.5
 LOOP_DELAY  = 1.0
 
 BASE = getattr(sys, '_MEIPASS', os.path.abspath('.'))
